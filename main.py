3b31678c437d049f2eb132ccd261062b0be295c7
antimonio124 <gfniscool@gmail.com>
2025-08-27 12:42:11 +0200

    B: better performance & added dt & fps

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +19,7 @@ def main():
 
 
         pygame.display.flip()   #END UPDATE
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
