477ec316f69f0cfa2972c3488872ac8eee8ec02a
antimonio124 <gfniscool@gmail.com>
2025-08-27 18:58:23 +0200

    C: render player

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+
+
 def main():
     pygame.init()
 
@@ -10,13 +13,14 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-
+        player.draw(screen)
 
         pygame.display.flip()   #END UPDATE
         dt = clock.tick(60)/1000
